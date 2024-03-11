7d41bf5f07ffd4b753c015cfcb1a756135f14eaf
Rohit Sharma <rohitdropbox25@gmail.com>
2024-03-11 09:47:25 +0300

    Started the repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Todo_go
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is very first project to build a Todo Web Application while learning Golang.")
+}
